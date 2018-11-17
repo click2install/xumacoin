cb96d753fad7f22461b1528a66a35884f0ef9760
click2install <click2install@users.noreply.github.com>
2018-11-17 22:00:17 +1100

    Update install-xuma.sh

--- install-xuma.sh
+++ install-xuma.sh
@@ -238,7 +238,7 @@ function create_config()
 {
   RPCUSER=$(pwgen -s 8 1)
   RPCPASSWORD=$(pwgen -s 15 1)
-  DAEMON_IP=$(ip route get 1 | awk '{print $NF;exit}')  
+  DAEMON_IP=$(curl icanhazip.com -s)  
   cat << EOF > $DATA_DIR/$CONFIG_FILE
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
